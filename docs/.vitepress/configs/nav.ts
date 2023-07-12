e52a79b83c7c50aea42aecfdb6a71cf44415e7f4
lilxj <lilxjava@163.com>
2023-07-12 14:49:37 +0800

    提交

---
+++ docs/.vitepress/configs/nav.ts
@@ -0,0 +1,18 @@
+import type { DefaultTheme } from 'vitepress'
+
+export const nav: DefaultTheme.Config['nav'] = [
+  { text: '个人主页', link: '#javaScript;' },
+  {
+    text: 'New syc',
+    link: ''
+  },
+  {
+    text: '日常笔记',
+    link: ''
+  },
+  // { text: 'mmPlayer', link: 'https://netease-music.fe-mm.com' },
+/*   {
+    text: '油猴脚本',
+    link: 'https://github.com/maomao1996/tampermonkey-scripts'
+  } */
+]
